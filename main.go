521ed041df16a7632559e715ee5f6df531c177bf
He Jun <>
2018-04-11 23:37:57 +0800

    http守护进程

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/fvbock/endless"
 )
 
+//SayHello is default response function
+func SayHello(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Hello"))
+}
+
 func main() {
-	fmt.Println("hello,rubik's cube!")
+
+	http.HandleFunc("/", SayHello)
+	err := endless.ListenAndServe(":8888", nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Server on 8888 stopped")
+
+	os.Exit(0)
 }
